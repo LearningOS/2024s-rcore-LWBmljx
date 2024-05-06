a25e74828b48d3b4142f2f7f0502e9812d844763
lwbmljx <luowb2004@gmail.com>
2024-05-06 15:18:09 +0800

    ch3 finish

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -58,7 +58,7 @@ pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
         *_ti = TaskInfo {
             status: TaskStatus::Running,
             syscall_times: task_info.syscall_counts,
-            time: get_time_us() - task_info.start_time,
+            time: (get_time_us() - task_info.start_time) / 1000,
         };
     }
     0
